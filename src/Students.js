9544a5f1f63b5b1f0b7474bacfa03a03e7c6d814
javierdillon21 <javierdillon21@gmail.com>
2020-12-01 18:37:45 -0500

    ModuloEstudiante-CorreccionCompleta

--- src/Students.js
+++ src/Students.js
@@ -2,45 +2,49 @@ import React, { useState } from 'react'
 import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import { listReservations } from './graphql/queries'
 import { updateReservation } from './graphql/mutations'
+import './App.css';
+
 
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
-
-function StudentInterface() {
-    var [data, setData] = useState(undefined)
-
-    async function MostrarReservas() {
-        const DataReservations = await API.graphql(graphqlOperation(listReservations))
-        const registros = DataReservations.data.listReservations.items
-        registros.forEach((dataobj) => {
-            var dia = dataobj.date
-            var timesobj = JSON.parse(dataobj.timeFrames)
-            var tabla = []
-            var intervalos = Object.keys(timesobj).map((intervalo) => {
-                return <div>{intervalo}</div>;
-            });
-            var columna = <div>
-                <div>{dia}</div>
-                <div>{intervalos}</div>
-            </div>
-            tabla.push(columna)
-        }
-        
-            return <div id="tabla"> {tabla} </div>
-        
-    }
-
-
-    // await API.graphql(graphqlOperation(updateReservation,{input:{} }))
-
-
-    return (<div>
-        <button onClick={async function () { MostrarReservas() }}>
-            ok
-        </button>
-        {data}
-    </div>)
+//Función que lee la BD y muestra el calendario de reservas
+async function MostrarReservas() {
+    const DataReservations = await API.graphql(graphqlOperation(listReservations))
+    const registros = DataReservations.data.listReservations.items
+    var tabla = []
+    registros.forEach((dataobj) => {
+        var dia = dataobj.date
+        var timesobj = JSON.parse(dataobj.timeFrames)
+        var intervalos = Object.keys(timesobj).map((intervalo) => {
+            return <div className="intervalo" onClick={() => {
+                //asignando 'reservado' al clickear un horario. Debe asignarse el id del usurario
+                timesobj[intervalo] = "reservado"
+                //testeo
+                console.log(intervalo + ': ' + timesobj[intervalo])
+            }}>{intervalo}</div>
+        });
+        var columna = <div>
+            <div>{dia}</div>
+            <div>{intervalos}</div>
+        </div>
+        tabla.push(columna)
+    })
+    return <div id="tabla">
+        {tabla}
+    </div>
 }
 
+
+
+//función principal 'Interface del estudiante'
+function StudentInterface() {
+    var [calendario, setCalendario] = useState(undefined)
+    return (<div>
+        <button onClick={async () => setCalendario(await MostrarReservas())}>
+            ok
+        </button>
+        {calendario}
+    </div>)
+}
 export default StudentInterface
